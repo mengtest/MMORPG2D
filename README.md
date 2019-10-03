ace28bb384e1f4243fc6019e590d166365471f3b
Lwm <2410250284@qq.com>
2019-10-03 23:34:38 +0800

    服务器分包处理

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MMORPG2D
 
 一个MMORPG2D游戏，嗯，就这样
+
+客户端采用python，基于pygame
+服务器采用golang，基于gonet
--- Server/server.go
+++ Server/server.go
@@ -6,6 +6,12 @@ import(
 	"fmt"
 )
 
+const(
+	CONSTHEADER       = "testHeader"
+    CONSTHEADERLENGTH = 10
+    CONSTMLENGTH      = 4
+)
+
 func main(){
 	Listening()
 }
@@ -34,13 +40,73 @@ func Listening(){
 func connHandle(conn net.Conn){
 	// 确保协程退出时候关闭
 	defer conn.Close()
+
+	var errs error
+	tempBuff := make([]byte,0)
 	readBuff := make([]byte,14)
+	data := make([]byte,20)
+
 	for{
 		n,err:=conn.Read(readBuff)
 		if err !=nil{
 			return
 		}
+		tempBuff = append(tempBuff,readBuff[:n]...)
+		// 对缓存器进行分包处理
+		tempBuff,data,errs = Depack(tempBuff)
+		if errs != nil {
+			return
+		}
+
+		if len(data)==0 {
+			return
+		}
 		// 读取客户端信息，注意这里是阻塞的！
 		fmt.Println(readBuff[:n])
+		//伪代码data是一个单独的包，可以进行逻辑处理了
+		// do(data)
 	}
 }
+
+func Enpack(message []byte) []byte{
+	return append(append([]byte(CONSTHEADER),IntToBytes(len(message))...),message...)
+}
+
+func Depack(buff[]byte)([]byte,[]byte,error){
+	length :=len(buff)
+
+	// 如果包长小于header 就直接返回，因为接受的数据不完整
+	if length<CONSTHEADERLENGTH+CONSTMLENGTH{
+		return buff,nil,nil
+	}
+
+	// 如果header不是指定的header说明数据已经被污染，则直接返回
+	if string(buff[:CONSTHEADERLENGTH])!=CONSTHEADER {
+		return []byte{},nil,error.New("header is not safe")
+	}
+	
+	msgLength := BytesToInt(buff[CONSTMLENGTH:CONSTHEADERLENGTH+CONSTMLENGTH])
+	if length < CONSTHEADERLENGTH + CONSTMLENGTH + msgLength{
+		return buff,nil,nil
+	}
+
+	data := buff[CONSTHEADERLENGTH + CONSTMLENGTH : CONSTHEADERLENGTH + CONSTMLENGTH + msgLength]
+	buffs := buff[CONSTHEADERLENGTH + CONSTMLENGTH + msgLength:]
+	return buffs,data,nil
+}
+
+//将int转成四个字节
+func IntToBytes(n int) []byte {
+    x := int32(n)
+    bytesBuffer := bytes.NewBuffer([]byte{})
+    binary.Write(bytesBuffer, binary.BigEndian, x)
+    return bytesBuffer.Bytes()
+}
+//将四个字节转成int
+func BytesToInt(b []byte)int {
+	bytesBuffer := bytes.NewBuffer(b)
+	var x int32
+	binary.Read(bytesBuffer,binary.BigEndian,&x)
+	return int(x)
+}
+
